900a80925f698a67910ffa7a18934bde7d4f9781
Александр Александров <aleks.aleksandrov@corp.mail.ru>
2023-06-08 11:33:02 +0000

    [MCSAD-1045] use new-help as npm package

---
+++ vkdocs.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    main: {
+        projectName: 'VK Cloud',
+        defaultLocale: 'ru',
+        useNativeSupportUrl: false
+    },
+    url: {
+        rootSitename: '/',
+        tgSupport: 'https://t.me/vk_tech_support_bot',
+        policy: '/additionals/start/legal/policy-privacy',
+    },
+}
