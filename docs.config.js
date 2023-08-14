2a94520d1debac3a4cbe8cb954cedc297ff16509
Service Account <github-sync-bot@corp.mail.ru>
2023-08-14 13:51:33 +0300

    git snapshot 14.08.2023 10:51

--- docs.config.js
+++ docs.config.js
@@ -2,7 +2,8 @@ export const config = {
     main: {
         projectName: 'VK Cloud',
         copyright: '© %year% VK Cloud',
-        analytics: null,
+        analytics: '(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return}}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,"script","https://mc.yandex.ru/metrika/tag.js","ym");ym(46889073,"init",{clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});',
+        scriptManager: 'https://vkcs-scripts.mrgcdn.ru/master/help.js',
     },
     breadcrumbs: {
         projectName: 'VK Cloud',
