58ee1d9bd4127a118418294a61ec881241ea6b61
a.yanchik <a.yanchik@vk.team>
2025-07-29 17:58:01 +0300

    ci: Bump to 5.0.0

--- config/audit.config.js
+++
@@ -1,7 +0,0 @@
-export const config = {
-    docsRelativePath: null,
-    dataRelativePath: null,
-    checkers: [
-        'LinksChecker', // Проверяем работоспособность ссылок в статьях документации
-    ],
-};
